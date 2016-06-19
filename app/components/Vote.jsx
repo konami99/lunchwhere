a46764c0db4783ded5e99d3f981d217c3a3760f2
Richard Chou <konami99@hotmail.com>
2016-06-20 03:32:34 +1000

    travel

--- app/components/Vote.jsx
+++ app/components/Vote.jsx
@@ -1,20 +1,38 @@
 var React = require('react');
 var {Link} = require('react-router');
+var PlacesMock = require('PlacesMock');
+
+var PlacesList = React.createClass({
+    render: function(){
+        var createRow = function(place){
+            return (
+                <div>
+                    <td>11</td>
+                    <td>22</td>
+                    <td>33</td>
+                </div>
+            );
+        };
+        return <tr>{PlacesMock.map(createRow)}</tr>;
+    }
+});
 
 var Vote = React.createClass({
     render: function(){
         return (
             <div>
-                <h1 className="text-center">Vote</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed lorem vel odio ornare suscipit. Pellentesque mi nulla, rhoncus ac mauris sit amet, tempor sagittis mi. Proin id eleifend nisi, sit amet pretium ex. Donec ac velit eu elit bibendum congue in id mauris. Ut pulvinar lacinia sollicitudin. Cras mattis dui vitae purus tincidunt maximus. Curabitur bibendum vitae sem vitae viverra. Nulla dictum sapien nibh, ut euismod mauris dictum ac. Duis quis ligula sit amet mauris eleifend convallis quis sed turpis. Cras imperdiet felis non velit condimentum, scelerisque facilisis nunc condimentum. Donec varius odio eget ex dignissim varius.</p>
-                <ol>
-                    <li>
-                        <Link to='/?location=Sydney'>Sydney</Link>
-                    </li>
-                    <li>    
-                        <Link to='/?location=Melbourne'>Melbourne</Link>
-                    </li>
-                </ol>
+                <table>
+                    <thead>
+                        <tr>
+                            <th width="200">Name</th>
+                            <th>Description</th>
+                            <th width="150">Options</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <PlacesList/>
+                    </tbody>
+                </table>
             </div>
         );
     }
