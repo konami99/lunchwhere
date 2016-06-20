5cf77f13b6fbb90707b3f2280c09902777ef4bf2
Richard Chou <konami99@hotmail.com>
2016-06-20 12:34:02 +1000

    lunch

--- app/components/PlacesMock.jsx
+++ app/components/PlacesMock.jsx
@@ -1,12 +1,28 @@
 var PlacesMock = [
     {
         id: 1,
-        name: "lorem ipsum",
+        name: "Sweet Salt",
         description: `
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Sweet Salt is located in trendy Redfern, and served up traditional fish and chips with a modern twist. Offering many health conscious options, it’s an ideal place for those who want to eat well without sacrificing big flavour. With takeout and dine-in available, it’s a quick, tasty, flavourful solution for the weekday dinner doldrums.. 
         `,
         votes: [1,2]
-    }
+    },
+    {
+        id: 2,
+        name: "Little Blue",
+        description: `
+        Little Blue - or St Kilda Pier Kiosk - has been a fixture in the Melbourne landscape since 1859 when the pier first opened to the public. Operating as a restaurant since 1904, the site became a staple destination for families, couples and visitors alike - an ideal place to watch the fairy penguins play and enjoy a bite. Since it’s rehabilitation, the restaurant has continued to captivate and charm with an emphasis on mod-Aus cuisine, locally sourced and organic meats, fish, and vegetables and an extensive wine, beer, and cocktail list.
+        `,
+        votes: [1,2]
+    },
+    {
+        id: 3,
+        name: "Michelinos Trattoria",
+        description: `
+        Since opening its doors 25 years ago, Michelinos Trattoria has become a favourite among hungry locals. Located within the Entertainment Quarter, the Italian restaurant has secured its place as the premier venue for corporate and quality functions. Michelino’s atmosphere, service, and innovative menu combine to offer diners an unforgettable Italian experience.
+        `,
+        votes: [1,2]
+    },
 
 
 ];
